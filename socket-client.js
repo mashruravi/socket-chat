ddff308f823b732a19cc780e2f1c3f6affd84134
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2017-05-20 17:19:10 +0530

    Handle client socket terminating connection erroneously

--- socket-client.js
+++ socket-client.js
@@ -3,8 +3,14 @@ let net = require("net");
 let HOST = "localhost";
 let PORT = 4321;
 
-let client = new net.Socket
+let client = new net.Socket();
 
 client.connect(PORT, HOST, () => {
 	console.log("Client connect listener");
+});
+
+client.on('data', (data) => {
+	console.log("Server says: " + data);
+
+	// client.destroy();
 });
--- socket-server.js
+++ socket-server.js
@@ -3,9 +3,47 @@ let net = require("net");
 let HOST = "localhost";
 let PORT = 4321;
 
+let count = 0;
+
+let connections = [];
+
+function broadcast(message) {
+	connections.forEach((e) => {
+		e.write(message);
+	});
+}
+
+function logConnections() {
+	connections.forEach((e, i) => {
+		console.log(i + " - " + e.remoteAddress + ":" + e.remotePort);
+	});
+}
+
 net.createServer((sock) => {
 
+	connections.push(sock);
+
 	console.log("Server received connection: "
-		+ sock);
+		+ sock.remoteAddress + ":" + sock.remotePort);
+	count++;
+
+	sock.write("You are client number " + count);
+
+	sock.on('end', () => {
+		console.log(sock.remoteAddress + ": " + sock.remotePort + " disconnected")
+	});
+
+	sock.on('error', (err) => {
+		// Remove sock from array of connections
+		logConnections();
+		let index = connections.indexOf(sock);
+		connections.splice(index, 1);		
+		console.log("After removing erroneous connection " + sock.remoteAddress + ":" + sock.remotePort + " at index " + index);
+		logConnections();
+	});
+
+	if (count == 2) {
+		broadcast("You are now two!");
+	}
 
 }).listen(PORT, HOST);
